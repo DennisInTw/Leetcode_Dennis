6f4f7248ec8d8e172422799e952ead61d2b1da29
DennisInTw <96195038+DennisInTw@users.noreply.github.com>
2022-01-07 22:50:50 +0800

    Create 206_Reverse_Linked_List.cpp in iteration solution

---
+++ 206_Reverse_Linked_List.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        // iteration solution
+        ListNode* prev;
+        ListNode* cur;
+        ListNode* next;
+        
+        prev = NULL;
+        cur = head;
+        
+        while (cur != NULL) {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        
+        head = prev;
+        
+        return head;
+    }
+};
