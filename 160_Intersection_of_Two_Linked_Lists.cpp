4ccbdf6ae51eee2d8dec40a1c6bcb94dda420053
DennisInTw <96195038+DennisInTw@users.noreply.github.com>
2022-01-12 00:18:48 +0800

    Refine the solution. Time complexity : O(m+n) , Space complexity : O(1)

--- 160_Intersection_of_Two_Linked_Lists.cpp
+++ 160_Intersection_of_Two_Linked_Lists.cpp
@@ -9,21 +9,46 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        // Time complexity : O(m*n)
-        ListNode* ptrA = headA;
-        ListNode* ptrB;
+        int lenA = 0, lenB = 0, diff = 0;
+        ListNode* curA = headA;
+        ListNode* curB = headB;
         
-        while (ptrA != NULL) {
-            ptrB = headB;
-            while (ptrB != NULL) {
-                if (ptrA == ptrB) {
-                    return ptrA;
-                }
-                
-                ptrB = ptrB->next;
+        while (curA != NULL) {
+            curA = curA->next;
+            lenA++;
+        }
+        
+        while (curB != NULL) {
+            curB = curB->next;
+            lenB++;
+        }
+        
+        curA = headA;
+        curB = headB;
+        
+        diff = (lenA - lenB >= 0) ? (lenA - lenB) : (lenB - lenA);
+        
+        if (lenA > lenB) {
+            // 讓curA之後的長度和curB對齊
+            while (diff > 0) {
+                curA = curA->next;
+                diff--;
+            }
+        } else if (lenA < lenB) {
+            // 讓curB之後的長度和curA對齊
+            while (diff > 0) {
+                curB = curB->next;
+                diff--;
+            }
+        }
+        
+        while (curA != NULL) {
+            if (curA == curB) {
+                return curA;
             }
             
-            ptrA = ptrA->next;
+            curA = curA->next;
+            curB = curB->next;
         }
         
         return NULL;
