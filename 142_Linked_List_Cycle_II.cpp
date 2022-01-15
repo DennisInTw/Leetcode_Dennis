de7de95a57f140ade923f8b185173e6fc4dfeeca
DennisInTw <96195038+DennisInTw@users.noreply.github.com>
2022-01-15 15:06:57 +0800

    Refine the solution by two pointers
    利用快慢指標來尋找是否有cycle,並且在找到cycle後再尋找entry node

--- 142_Linked_List_Cycle_II.cpp
+++ 142_Linked_List_Cycle_II.cpp
@@ -8,38 +8,36 @@
  */
 class Solution {
 public:
+    ListNode* moveForward(ListNode* node, int nums) {
+        while (node != NULL && (nums--) > 0) {
+            node = node->next;
+        }
+        
+        return node;
+    }
+    
     ListNode *detectCycle(ListNode *head) {
-        /* 將經過的node的val改成stopVal,下次遇到node的val為stopVal就表示為cycle的點
-           最後再將儲存起來的val還原
-           Space complexity : O(node個數)
-        */
-        int nodeVal[10000];
-        int i = 0;
-        ListNode* ans = NULL;
-        ListNode* cur = head, *tmp = head;
-        const int stopVal = -100001;
+        // two pointers solution
+        ListNode* fast = head;
+        ListNode* slow = head;
         
-        while (cur != NULL) {
-            if (cur->val == stopVal) {
-                ans = cur;
-                break;
-            }
-            nodeVal[i++] = cur->val; // 將node的val儲存起來
-            cur->val = stopVal;
-            cur = cur->next;
-        }
-        
-        i = 0;
-        // recover node value
-        while (tmp != NULL) {
-            if (tmp->val == stopVal) {
-                tmp->val = nodeVal[i++];
-                tmp = tmp->next;
-            } else {
+        // Check if exists a cycle
+        while (fast != NULL) {
+            fast = moveForward(fast, 2); // move forward 2 nodes
+            slow = moveForward(slow, 1);  // move forward 1 node
+            if (fast == slow) { // fast meets slow in the cycle
                 break;
             }
         }
         
-        return ans;
+        if (fast != NULL) { // exists a cycle
+            slow = head;
+            while (fast != slow) { // find the entry node of the cycle
+                fast = moveForward(fast, 1);
+                slow = moveForward(slow, 1);
+            }
+        }
+        
+        return fast;
     }
 };
