5f3cd5cf846930a63193e16075279c5f24e8967a
Dennis Wong <96195038+DennisInTw@users.noreply.github.com>
2022-01-17 23:50:30 +0800

    Create 454_4Sum_II.cpp Use hash solution, time complexity = O(n^2), but still not good

---
+++ 454_4Sum_II.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        // Brute force : time complexity = O(n^4)
+        // Hash : time complexity = O(n^2 + n^2 + n^2)
+        
+        unordered_map<int,int> map_sum1_2;
+        unordered_map<int,int> map_sum3_4;
+        int len = nums1.size();
+        int ans = 0;
+        
+        // O(n^2)
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                int sum = nums1[i] + nums2[j];
+                if (map_sum1_2.count(sum) == 0) {
+                    map_sum1_2.insert(make_pair(sum, 1));
+                } else {
+                    map_sum1_2[sum]++;
+                }
+                
+            }
+        }
+        
+        // O(n^2)
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                int sum = nums3[i] + nums4[j];
+                if (map_sum3_4.count(sum) == 0) {
+                    map_sum3_4.insert(make_pair(sum, 1));
+                } else {
+                    map_sum3_4[sum]++;
+                }
+                
+            }
+        }
+
+        // O(n^2)
+        for (auto& n : map_sum1_2) {
+            int sum = n.first;
+            int count = n.second;
+            if (map_sum3_4.count(0 - sum) == 1) {
+                ans += (count * map_sum3_4[0 - sum]);
+            }
+        }
+        
+        return ans;
+    }
+};
