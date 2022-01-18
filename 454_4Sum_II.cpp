9608df580eb010c398988cd620cee3819db58df6
Dennis Wong <96195038+DennisInTw@users.noreply.github.com>
2022-01-18 21:51:51 +0800

    Refine the code to time complexity = O(n^2 + n^2)

--- 454_4Sum_II.cpp
+++ 454_4Sum_II.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         // Brute force : time complexity = O(n^4)
-        // Hash : time complexity = O(n^2 + n^2 + n^2)
+        // Hash : time complexity = O(n^2 + n^2)
         
         unordered_map<int,int> map_sum1_2;
         unordered_map<int,int> map_sum3_4;
@@ -21,16 +21,10 @@ public:
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len; j++) {
                 int sum = nums3[i] + nums4[j];
-                map_sum3_4[sum]++;
-            }
-        }
-
-        // O(n^2)
-        for (auto& n : map_sum1_2) {
-            int sum = n.first;
-            int count = n.second;
-            if (map_sum3_4.count(0 - sum) == 1) {
-                ans += (count * map_sum3_4[0 - sum]);
+                //if (map_sum1_2.count(0 - sum) == 1) {
+                if (map_sum1_2.find(0 - sum) != map_sum1_2.end()) {
+                    ans += (map_sum1_2[0 - sum]);
+                }
             }
         }
         
