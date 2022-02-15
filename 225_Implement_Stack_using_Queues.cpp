df7c34ff2e2356d0865e52ed22968eab5f9885ab
Dennis Wong <96195038+DennisInTw@users.noreply.github.com>
2022-02-16 00:00:44 +0800

    Create 225_Implement_Stack_using_Queues.cpp with 1 queue

--- 225_Implement_Stack_using_Queues.cpp
+++ 225_Implement_Stack_using_Queues.cpp
@@ -1,47 +1,40 @@
 class MyStack {
 public:
-    // 用兩個queue來模擬stack
-    queue<int> que1;
-    queue<int> que2;
+    // 用一個queue來模擬stack
+    queue<int> que;
     
     MyStack() {
         
     }
     
-    // 直接將x加入到que1
+    // 直接將x加入到que
     void push(int x) {
-        que1.push(x);    
+        que.push(x);    
     }
     
     int pop() {
         int val;
-        int size = que1.size();
+        int size = que.size();
         
-        // 將que1的元素(除了最後一個元素)移到que2
+        // 將que的前面元素都放到que的最後, 然後再移除元素, 一直到一開始的que的最後一個元素
         while (size-- > 1) {
-            que2.push(que1.front());
-            que1.pop();
+            que.push(que.front());
+            que.pop();
         }
         
-        // que1只剩一個元素, 將他pop出來
-        val = que1.front();
-        que1.pop();
-        
-        // 再將que2元素移到que1
-        while (!que2.empty()) {
-            que1.push(que2.front());
-            que2.pop();
-        }
+        // que的front就是當初的最後一個元素
+        val = que.front();
+        que.pop();
         return val;
     }
     
     int top() {
-        // que1最後一個元素就是stack的top
-        return que1.back();
+        // que最後一個元素就是stack的top
+        return que.back();
     }
     
     bool empty() {
-        return que1.empty();
+        return que.empty();
     }
 };
 
